d702787586527feddafe685b75f88fcbd75c132e
Eskeyz <alexjak56@gmail.com>
2023-08-16 21:41:47 +0700

    fix: Loader

--- Loader.lua
+++ Loader.lua
@@ -1,7 +1,7 @@
 if game.PlaceId == 13989423944 then
-    --loadstring
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Eskeyz/Loader/main/Games/obbyadvanture.lua"))()
 elseif game.PlaceId == 23555523533223 then
-    --loadstring
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Eskeyz/Loader/main/Games/obbyadvanture.lua"))()
 else
     game.Players.LocalPlayer:Kick("Game not Support! - Eskey")
 end
