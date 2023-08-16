6493a26ee545d01f96e88044f764adaecf09aa9a
Eskeyz <alexjak56@gmail.com>
2023-08-17 06:59:11 +0700

    feat: Add Boxing

--- Loader.lua
+++ Loader.lua
@@ -8,6 +8,8 @@ elseif game.PlaceId == 10956766913 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Eskeyz/Loader/main/Games/shein.lua"))()
 elseif game.PlaceId == 13464725108 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Eskeyz/Loader/main/Games/hithop.lua"))()
+elseif game.PlaceId == 10857807258 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Eskeyz/Loader/main/Games/boxing.lua"))()
 else
     game.Players.LocalPlayer:Kick("Game not Support! - Eskey")
 end
