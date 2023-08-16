f80ddd7bc9e91134d15a256fd6b1e317debae923
Eskeyz <alexjak56@gmail.com>
2023-08-16 22:08:34 +0700

    feat: Add Aec, Shein, Hithoop

--- Loader.lua
+++ Loader.lua
@@ -2,6 +2,12 @@ if game.PlaceId == 13989423944 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Eskeyz/Loader/main/Games/obbyadvanture.lua"))()
 elseif game.PlaceId == 10676523834 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Eskeyz/Loader/main/Games/racingrocket.lua"))()
+elseif game.PlaceId == 13370783664 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Eskeyz/Loader/main/Games/animeenergy.lua"))()
+elseif game.PlaceId == 10956766913 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Eskeyz/Loader/main/Games/shein.lua"))()
+elseif game.PlaceId == 13464725108 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Eskeyz/Loader/main/Games/hithop.lua"))()
 else
     game.Players.LocalPlayer:Kick("Game not Support! - Eskey")
 end
