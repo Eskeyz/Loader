2689bf2160ff0ba192b03f47dc57c9ff912424f2
Eskeyz <alexjak56@gmail.com>
2023-08-16 21:39:18 +0700

    feat: add Obby Advanture

---
+++ Loader.lua
@@ -0,0 +1,7 @@
+if game.PlaceId == 13989423944 then
+    --loadstring
+elseif game.PlaceId == 23555523533223 then
+    --loadstring
+else
+    game.Players.LocalPlayer:Kick("Game not Support! - Eskey")
+end
