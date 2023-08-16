c3888bb0db14e750bbdecfdf7361f2975e88c36b
Eskeyz <alexjak56@gmail.com>
2023-08-16 21:54:15 +0700

    feat: add Racing Rocket

--- Loader.lua
+++ Loader.lua
@@ -1,7 +1,7 @@
 if game.PlaceId == 13989423944 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Eskeyz/Loader/main/Games/obbyadvanture.lua"))()
-elseif game.PlaceId == 23555523533223 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Eskeyz/Loader/main/Games/obbyadvanture.lua"))()
+elseif game.PlaceId == 10676523834 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Eskeyz/Loader/main/Games/racingrocket.lua"))()
 else
     game.Players.LocalPlayer:Kick("Game not Support! - Eskey")
 end
